7927d7d01385ef8abdd69c4bd18919daa59320e5
HyvAchoK <boxofdem@gmail.com>
2020-11-06 21:25:17 +0300

    initial commit

---
+++ rke/init-node.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo -e "test\ntest" | passwd root >/dev/null 2>&1
+
+# Disable ufw
+ufw disable
+
+# Disable swap
+swapoff -a; sed -i '/swap/d' /etc/fstab
+
+# Edit sysctl
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+# Install docker engine
+apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update && apt install -y docker-cecontainerd.io
